c0f5b0938f41faaa6e5bbbae7d4c68d64652998a
Vylich <ilya.sidorenko.99@gmail.com>
2023-11-19 14:47:13 +0300

    .

--- dist/index.js
+++ dist/index.js
@@ -26,7 +26,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static('uploads'));
+app.use('/upload', express.static('uploads'));
 app.get('/', (req, res) => {
     res.send('Hello pidor!');
 });
--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ const upload = multer({ storage });
 
 app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static('uploads'));
+app.use('/upload', express.static('uploads'));
 
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello pidor!');
